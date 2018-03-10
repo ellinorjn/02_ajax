24258744236ffbe61543a3258219f76022e3e132
ellinorjn <ellinor.jansson@medieinstitutet.se>
2018-03-10 10:31:31 +0100

    added information about the searched movie

--- js/main.js
+++ js/main.js
@@ -1,30 +1,36 @@
 const searchMovie = document.getElementById('searchMovie')
+const searchMovieButton = document.getElementById('searchMovieButton')
 
-searchMovie.addEventListener('change', function(){
+searchMovie.addEventListener('keyup', function (e) {
     const searchValue = searchMovie.value;
-    getSearchedMovie(searchValue)
-})
+    if (e.keyCode === 13) {
+        getSearchedMovie(searchValue)
+    }
+});
+
+//searchMovieButton.addEventListener('click', getSearchedMovie)
 
 getSearchedMovie();
 
-function getSearchedMovie(movie = "remember+me"){
-    fetch('http://www.omdbapi.com/?apikey=da783fad&t=' + movie +'')
-    .then((response) => response.json())
-    .then((movies)=> {
-        console.log(movies)
-        displayMovies(movies)
-    })
-    .catch(function(error){
-        console.log(error);
-    })
+
+function getSearchedMovie(movie = "remember+me") {
+    fetch('http://www.omdbapi.com/?apikey=da783fad&t=' + movie + '')
+        .then((response) => response.json())
+        .then((movies) => {
+            console.log(movies)
+            displayMovies(movies)
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+    searchMovie.value = "";
 }
 
 function displayMovies(movies) {
     const visaFilm = document.getElementById('test')
-    let movieInfo = `<p>${movies.Title}</p><p>${movies.Year}</p>`;
+    let movieInfo = `<h2>${movies.Title}</h2>
+                    <p>Released: ${movies.Released}</p>
+                    <p>${movies.Plot}</p>
+                    <p>IMDb rating: ${movies.imdbRating}</p>`;
     movieInformation.innerHTML = movieInfo;
 }
-
-
-
-
