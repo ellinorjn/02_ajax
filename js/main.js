e150c8a5e207d23e57d36dbc010dcf74df782485
ellinorjn <ellinor.jansson@medieinstitutet.se>
2018-03-12 14:05:13 +0100

    styleing

--- js/main.js
+++ js/main.js
@@ -10,13 +10,12 @@ function getSearchedMovie(movie){
         //ha en tom array när man gör en ny sökning
         globalMovieArray.length=0;
         globalMovieArray.push(movies);
-        //globalMovie = movies;
         console.log(movies)
         displayMovies(movies)
     })
     .catch(function (error) {
             console.log(error);
-        })
+    })
     searchMovie.value = "";
 }
 
@@ -27,73 +26,40 @@ searchMovie.addEventListener('keyup', function(e){
     }
 });
 
-//getSearchedMovie();
-/*searchMovie.addEventListener('keyup', function (e) {
-    const searchValue = searchMovie.value;
-    if (e.keyCode === 13) {
-        getSearchedMovie(searchValue)
-    }
-});*/
-
 //searchMovieButton.addEventListener('click', getSearchedMovie)
-
-
-
-
-/*
-function getSearchedMovie(movie = "remember me") {
-    fetch('http://www.omdbapi.com/?apikey=da783fad&t=' + movie + '')
-        .then((response) => response.json())
-        .then(function (movies) {
-            globalMovie = movies;
-            console.log(movies)
-            displayMovies(movies)
-            buttonReadMore(movieInformation)
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
-    searchMovie.value = "";
-}*/
-
 
 const movieInformation = document.getElementById('movieInformation')
 
 function displayMovies(movies){
     console.log(globalMovieArray);
-    let test = globalMovieArray[0].Search;
-    let movieTitle = '';
-    for(i=0; i < test.length; i++){
-        movieTitle += `<li>${movies.Search[i].Title}</li>`
-        movieInformation.innerHTML = movieTitle
+    let searchedMovies = globalMovieArray[0].Search;
+    for(i=0; i < searchedMovies.length; i++){
+        let movieTitleListed = document.createElement('li');
+        let movieTitle = document.createTextNode(`${movies.Search[i].Title}`)
+        movieTitleListed.appendChild(movieTitle);
+        movieInformation.appendChild(movieTitleListed);
+        
+        buttonForMoreInformation()
         console.log(movies.Search[i].Title);
     }
 }
+
+function buttonForMoreInformation(){
+    const moreInformationButton = document.createElement('button');
+    moreInformationButton.className = "moreInformationButton";
+    const textMoreInformationButton = document.createTextNode('More info.')
+    moreInformationButton.appendChild(textMoreInformationButton)
+    movieInformation.appendChild(moreInformationButton);
     
-    /*
-    for(i = 0; i < movies.length; i++){
-    const movieInfo = document.createTextNode(`${movies.Title[i]}`)
-    //let movieInfo = `<h2>${movies.Title[i]}</h2>`        
-    movieInformation.appendChild(movieInfo);
-}}*/
-
-/* Read more button 
-function buttonReadMore(movieInformation) {
-    const readMoreButton = document.createElement('button');
-    readMoreButton.className = "readMoreButton"
-    const textReadMoreButton = document.createTextNode('Read more')
-    readMoreButton.appendChild(textReadMoreButton)
-    movieInformation.appendChild(readMoreButton);
-
-    readMoreButton.addEventListener('click', function () {
-        console.log("hej");
-        readMore(globalMovie);
+    moreInformationButton.addEventListener('click', function(){
+        console.log("hellu");
     })
 }
 
-function readMore(globalMovie) {
-    let moreInformation = document.createTextNode(`${globalMovie.Country}
-                                                    IMDb rating: ${globalMovie.imdbRating}
-                                                    Genre: ${globalMovie.Genre}`)
-    movieInformation.appendChild(moreInformation)
+/*function getMovieImdbId(){
+    fetch('http://www.omdbapi.com/?apikey=da783fad&i=' + NÅGONTING HÄR +'')
+          .then((response) => json())
+          .then ((imdbId) => {
+          console.log(imdbID);
+          })
 }*/
